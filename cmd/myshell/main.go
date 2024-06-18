37402ad207c801936a3f665819fa1149738fa43b
Andrew Schmidt <adavidschmidt@gmail.com>
2024-06-18 16:54:20 -0400

    pass 1st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bufio"
-	// Uncomment this block to pass the first stage
-	// "fmt"
+	//Uncomment this block to pass the first stage
+	"fmt"
 	"os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	//Uncomment this block to pass the first stage
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
