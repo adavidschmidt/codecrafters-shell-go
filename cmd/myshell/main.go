272ed89b632861ba49b41649a5a0c54e6a8f0a59
Andrew Schmidt <adavidschmidt@gmail.com>
2024-06-18 17:45:42 -0400

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,11 +14,8 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := reader.ReadString('\n')
+		command, _ := reader.ReadString('\n')
 		command = strings.TrimSpace(command)
-		if err != nil {
-			fmt.Printf("%s: command not found\n", strings.TrimRight(command, "\n"))
-		}
-		
+		fmt.Printf("%s: command not found\n", strings.TrimRight(command, "\n"))
 	}
 }
