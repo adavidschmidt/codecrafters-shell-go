a3349dd8f570f125701ccd94d9e608bce67f99db
Andrew Schmidt <adavidschmidt@gmail.com>
2024-06-18 17:51:01 -0400

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,6 +16,9 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := reader.ReadString('\n')
 		command = strings.TrimSpace(command)
+		if command == "exit 0" {
+			os.exit(0)
+		}
 		fmt.Printf("%s: command not found\n", strings.TrimRight(command, "\n"))
 	}
 }
