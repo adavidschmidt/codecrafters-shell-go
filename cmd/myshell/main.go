ab8385fca26e063b3bff20346235acb69df897fb
Andrew Schmidt <adavidschmidt@gmail.com>
2024-06-18 23:07:20 -0400

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -27,6 +27,7 @@ func main() {
 		command, _ := reader.ReadString('\n')
 		tokens := strings.Fields(strings.TrimSpace(command))
 		command = strings.Join(tokens, " ")
+		path := strings.Split(os.Getenv("PATH"), ":")
 		switch {
 		case tokens[0] == "exit" && tokens[1] == "0":
 				os.Exit(0)
@@ -37,8 +38,8 @@ func main() {
 		case tokens[0] == "type" && stringInSlice(tokens[1], listCommands):
 			fmt.Printf("%s is a shell builtin\n", tokens[1])
 		
-		case tokens[0] == "type" && strings.Contains(tokens[1], os.Getenv(PATH)):
-			fmt.Printf("%s is %s", tokens[1], os.Getenv(PATH)) 
+		case tokens[0] == "type" && strings.Contains(tokens[1], path):
+			fmt.Printf("%s is %s", tokens[1], path) 
 		
 		case tokens[0] == "type" && !stringInSlice(tokens[1], listCommands):
 			fmt.Printf("%s: not found\n", tokens[1])
